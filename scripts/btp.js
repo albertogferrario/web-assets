45bdbf542310c723b3245b4d6192ffc69c6db1aa
Alberto Giancarlo Ferrario <me@albertogferrar.io>
2024-07-29 13:15:48 +0700

    refactor

--- scripts/btp.js
+++ scripts/btp.js
@@ -1,6 +1,6 @@
 (function() {
     const script = document.createElement('script');
-    script.src = 'scripts/btp.min.js?t=' + new Date().getTime();
+    script.src = 'btp.min.js?t=' + new Date().getTime();
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
 })();
