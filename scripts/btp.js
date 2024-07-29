061b48f120559408533f5f64cb110528b2fe5ec1
Alberto Giancarlo Ferrario <me@albertogferrar.io>
2024-07-29 17:36:26 +0700

    up btp

--- scripts/btp.js
+++ scripts/btp.js
@@ -2,5 +2,6 @@
     const script = document.createElement('script');
     script.src = 'https://assets.web.albertogferrar.io/scripts/btp.min.js?t=' + new Date().getTime();
     script.type = 'text/javascript';
+    script.defer = true;
     document.getElementsByTagName('head')[0].appendChild(script);
 })();
